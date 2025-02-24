9d849ec4d9bee61bcb5119fda9aa92316426bae7
91tleg <maxchou0417@gmail.com>
2025-02-23 19:32:15 -0800

    move btn  pin definition to here

--- src/btn.cpp
+++ src/btn.cpp
@@ -1,7 +1,4 @@
-#include <Arduino.h>
-#include <stdint.h>
 #include "btn.h"
-#include "pindef.h"
 
 uint8_t btn1 = LOW;
 uint8_t btn1_prev = LOW;
@@ -12,16 +9,10 @@ void btn_init(void)
 {
     pinMode(BUTTON1_PIN, INPUT_PULLUP);
     pinMode(BUTTON2_PIN, INPUT_PULLUP);
-
-     #if PRINT_DEBUG_MESSAGES_ON_LCD
-        lcd.print("btn set up done");
-    #endif
 }
 
-void read_btns(void)
+void btn_read(void)
 {
     btn1 = digitalRead(BUTTON1_PIN);
-    btn1_prev = digitalRead(BUTTON1_PIN);
     btn2 = digitalRead(BUTTON2_PIN);
-    btn2_prev = digitalRead(BUTTON2_PIN);
 }
--- src/btn.h
+++ src/btn.h
@@ -1,7 +1,19 @@
 #ifndef BTN_H
 #define BTN_H
 
-void btn_init(void);
-void read_btns(void);
+#include <Arduino.h>
+#include <stdint.h>
+#include "definitions.h"
 
-#endif
+#define BUTTON1_PIN 22
+#define BUTTON2_PIN 24
+
+void btn_init(void);
+void btn_read(void);
+
+extern uint8_t btn1;
+extern uint8_t btn1_prev;
+extern uint8_t btn2;
+extern uint8_t btn2_prev;
+
+#endif // BTN_H
