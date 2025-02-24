657dc33f0ea9d13f6b9647b1d9bc5466656f5a07
91tleg <maxchou0417@gmail.com>
2025-02-23 19:35:41 -0800

    add structs initialization

--- src/state_diag.cpp
+++ src/state_diag.cpp
@@ -1,3 +1,8 @@
 #include "state_diag.h"
 
+input_switches status = {0};
+io_switches status0 = {0};
+trouble_code_one status1 = {0};
+trouble_code_two status2 = {0};
+trouble_code_three status3 = {0};
 DIAG diag_current = DIAG::IN;
