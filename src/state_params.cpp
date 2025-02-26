6a5da3613b3b3a228d1433d1c434282c01394e51
91tleg <maxchou0417@gmail.com>
2025-02-25 19:54:01 -0800

    add more read methods for better control and read only what we need

--- src/state_params.cpp
+++ src/state_params.cpp
@@ -1,52 +1,52 @@
 #include "state_params.h"
 
+ecu_params ecu_parameters = {0};
 PARAMS param_current = PARAMS::PARAMS_ONE;
-extern ecu_params ecu_parameters;
-
-void display_romid(LiquidCrystal_I2C *lcd)
-{
-    for (uint8_t i = 0; i < 3; ++i)
-    {
-        if (ecu_parameters.romid_param[i] < 0x10)
-        {
-            lcd->print(0); // Print leading zero for single-digit hex values
-        }
-        lcd->print(ecu_parameters.romid_param[i], HEX);
-        if (i < 2)
-        {
-            lcd->write(static_cast<uint8_t>('.'));
-        }
-    }
-}
 
 void read_sensor_one(void)
 {
     read_battery_voltage();
     read_speed();
-    read_rpm();
-    read_coolant_temp();
 }
 
 void read_sensor_two(void)
 {
-    read_fuel_trim();
-    read_airflow();
-    read_load();
-    read_throttle_percentage();
+    read_rpm();
+    read_coolant_temp();
 }
 
 void read_sensor_three(void)
 {
-    read_injector_pulse_width();
-    read_iacv_duty_cycle();
-    read_o2_signal();
-    read_timing_correction();
+    read_fuel_trim();
+    read_airflow();
 }
 
 void read_sensor_four(void)
 {
     read_atmosphere_pressure();
     read_manifold_pressure();
+}
+
+void read_sensor_five(void)
+{
+    read_injector_pulse_width();
+    read_iacv_duty_cycle();
+}
+
+void read_sensor_six(void)
+{
+    read_load();
+    read_throttle_percentage();
+}
+
+void read_sensor_seven(void)
+{
+    read_o2_signal();
+    read_timing_correction();
+}
+
+void read_sensor_eight(void)
+{
     read_boost_control_duty_cycle();
     read_throttle_signal();
 }
