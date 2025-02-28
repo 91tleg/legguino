2b05158dd5af8722a35435c8800398925fba4723
91tleg <maxchou0417@gmail.com>
2025-02-27 20:13:48 -0800

    add scan page control

--- src/state_scan.cpp
+++ src/state_scan.cpp
@@ -1,3 +1,18 @@
 #include "state_scan.h"
-
+#include "btn.h"
 SCAN lcd_current_page = SCAN::SENSOR_ONE;
+
+void scan_page_control(void)
+{
+    if (lcd_current_page > SCAN::CLEAR_TC)
+        lcd_current_page = SCAN::SENSOR_ONE;
+    btn1 = digitalRead(BUTTON1_PIN);
+    if (btn1 == LOW && btn1_prev == HIGH)
+    {
+        PORTB |= (1 << PB7);
+        _delay_ms(100);
+        PORTB &= ~(1 << PB7);
+        lcd_current_page = static_cast<SCAN>(static_cast<uint8_t>(lcd_current_page) + 1);
+    }
+    btn1_prev = digitalRead(BUTTON1_PIN);
+}
