4edc1443d6058d02476e256167965a31e32fb6e8
91tleg <maxchou0417@gmail.com>
2025-02-27 21:08:22 -0800

    add more pages to scan to cover more sensors

--- src/state_scan.h
+++ src/state_scan.h
@@ -10,16 +10,22 @@ enum class SCAN : uint8_t
   /* 01 */ SENSOR_TWO,
   /* 02 */ SENSOR_THREE,
   /* 03 */ SENSOR_FOUR,
-  /* 04 */ INPUT_SW,
-  /* 05 */ IO_SW,
-  /* 06 */ ACTIVE_TC_ONE,
-  /* 07 */ ACTIVE_TC_TWO,
-  /* 08 */ ACTIVE_TC_THREE,
-  /* 09 */ STORED_TC_ONE,
-  /* 10 */ STORED_TC_TWO,
-  /* 11 */ STORED_TC_THREE,
-  /* 12 */ CLEAR_TC
+  /* 04 */ SENSOR_FIVE,
+  /* 05 */ SENSOR_SIX,
+  /* 06 */ SENSOR_SEVEN,
+  /* 07 */ SENSOR_EIGHT,
+  /* 08 */ INPUT_SW,
+  /* 09 */ IO_SW,
+  /* 10 */ ACTIVE_TC_ONE,
+  /* 11 */ ACTIVE_TC_TWO,
+  /* 12 */ ACTIVE_TC_THREE,
+  /* 13 */ STORED_TC_ONE,
+  /* 14 */ STORED_TC_TWO,
+  /* 15 */ STORED_TC_THREE,
+  /* 16 */ CLEAR_TC
 };
+
+void scan_page_control(void);
 
 extern SCAN lcd_current_page;
 
