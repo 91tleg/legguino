e0181e459d21ebc03725ca40d7b8960bd81121a7
91tleg <maxchou0417@gmail.com>
2025-03-03 21:43:07 -0800

    Add button press detection with debouncing

--- src/btn.cpp
+++ src/btn.cpp
@@ -1,5 +1,8 @@
 #include "btn.h"
 
+
+BUTTON_STATE button = {};
+
 uint8_t btn1 = LOW;
 uint8_t btn1_prev = LOW;
 uint8_t btn2 = LOW;
@@ -16,3 +19,51 @@ void btn_read(void)
     btn1 = digitalRead(BUTTON1_PIN);
     btn2 = digitalRead(BUTTON2_PIN);
 }
+
+BUTTON_STATE get_button_state()
+{
+    static int btn1_prev = HIGH;
+    static unsigned long pressedTime = 0;
+    
+    int btn1 = digitalRead(BUTTON1_PIN); // Read button state
+    unsigned long currentMillis = millis();
+
+    if (btn1 == LOW && btn1_prev == HIGH) // Button just pressed
+    {
+        pressedTime = currentMillis;
+        btn1_prev = LOW;
+    }
+    else if (btn1 == HIGH && btn1_prev == LOW) // Button just released
+    {
+        btn1_prev = HIGH;
+        if (currentMillis - pressedTime >= LONG_PRESS_TIME)
+        {
+            return BUTTON_STATE::LONG_PRESS; // Long press detected
+        }
+        else
+        {
+            return BUTTON_STATE::SHORT_PRESS; // Short press detected
+        }
+    }
+
+    return BUTTON_STATE::OFF; // No press detected
+}
+
+bool button_pressed(void)
+{
+    static unsigned long lastPressTime = 0;
+    btn1 = digitalRead(BUTTON1_PIN);
+    if (btn1 == LOW && btn1_prev == HIGH)
+    {
+        if (millis() - lastPressTime > DEBOUNCE_DELAY) // Debounce check
+        {
+            lastPressTime = millis();
+            btn1_prev = LOW;
+            return true;
+        }
+    }
+    btn1_prev = btn1;
+    return false;
+}
+
+
--- src/btn.h
+++ src/btn.h
@@ -3,17 +3,31 @@
 
 #include <Arduino.h>
 #include <stdint.h>
-#include "definitions.h"
+#include "state_menu.h"
+#include "legguino.h"
 
 #define BUTTON1_PIN 22
 #define BUTTON2_PIN 24
+#define SHORT_PRESS_TIME 500
+#define LONG_PRESS_TIME 1000
+#define DEBOUNCE_DELAY 50
+
+enum class BUTTON_STATE : uint8_t
+{
+	OFF,
+	SHORT_PRESS,
+	LONG_PRESS
+};
 
 void btn_init(void);
 void btn_read(void);
+BUTTON_STATE get_button_state(void);
+bool button_pressed(void);
 
 extern uint8_t btn1;
 extern uint8_t btn1_prev;
 extern uint8_t btn2;
 extern uint8_t btn2_prev;
+extern BUTTON_STATE button;
 
 #endif // BTN_H
