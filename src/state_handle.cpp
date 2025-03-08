6455eaa261eb53b3c332254e6d9e5515a6555576
91tleg <maxchou0417@gmail.com>
2025-03-07 20:11:48 -0800

    initial commit -Added function declarations for handling sensors, input switches, active and stored codes, and race modes. These functions will be used to manage different system states and transitions.

---
+++ src/state_handle.cpp
@@ -0,0 +1,115 @@
+#include "state_handle.h"
+
+void handle_sensor_one(void)
+{
+    read_sensor_one();
+    lcd_print_params_one();
+}
+
+void handle_sensor_two(void)
+{
+    read_sensor_two();
+    lcd_print_params_two();
+}
+
+void handle_sensor_three(void)
+{
+    read_sensor_three();
+    lcd_print_params_three();
+}
+
+void handle_sensor_four(void)
+{
+    read_sensor_four();
+    lcd_print_params_four();
+}
+
+void handle_sensor_five(void)
+{
+    read_sensor_five();
+    lcd_print_params_five();
+}
+
+void handle_sensor_six(void)
+{
+    read_sensor_six();
+    lcd_print_params_six();
+}
+
+void handle_sensor_seven(void)
+{
+    read_sensor_seven();
+    lcd_print_params_seven();
+}
+
+void handle_sensor_eight(void)
+{
+    read_sensor_eight();
+    lcd_print_params_eight();
+}
+
+void handle_input_sw(void)
+{
+    read_input_switches();
+    lcd_print_input_sw();
+}
+
+void handle_io_sw(void)
+{
+    read_io_switches();
+    lcd_print_io_sw();
+}
+
+void handle_active_one(void)
+{
+    read_active_trouble_code_one();
+    lcd_print_code_one();
+}
+
+void handle_active_two(void)
+{
+    read_active_trouble_code_two();
+    lcd_print_code_two();
+}
+
+void handle_active_three(void)
+{
+    read_active_trouble_code_three();
+    lcd_print_code_three();
+}
+
+void handle_stored_one(void)
+{
+    read_stored_trouble_code_one();
+    lcd_print_code_one();
+}
+
+void handle_stored_two(void)
+{
+    read_stored_trouble_code_two();
+    lcd_print_code_two();
+}
+
+void handle_stored_three(void)
+{
+    read_stored_trouble_code_three();
+    lcd_print_code_three();
+}
+
+void handle_race_one(void)
+{
+    read_coolant_temp();
+    lcd_print_tw();
+}
+
+void handle_race_two(void)
+{
+    read_throttle_percentage();
+    lcd_print_tps();
+}
+
+void handle_race_three(void)
+{
+    read_manifold_pressure();
+    lcd_print_mp();
+}
---
+++ src/state_handle.h
@@ -0,0 +1,26 @@
+#ifndef STATE_HANDLE_H
+#define STATE_HANDLE_H
+#include "state_params.h"
+#include "state_diag.h"
+
+void handle_sensor_one(void);
+void handle_sensor_two(void);
+void handle_sensor_three(void);
+void handle_sensor_four(void);
+void handle_sensor_five(void);
+void handle_sensor_six(void);
+void handle_sensor_seven(void);
+void handle_sensor_eight(void);
+void handle_input_sw(void);
+void handle_io_sw(void);
+void handle_active_one(void);
+void handle_active_two(void);
+void handle_active_three(void);
+void handle_stored_one(void);
+void handle_stored_two(void);
+void handle_stored_three(void);
+void handle_race_one(void);
+void handle_race_two(void);
+void handle_race_three(void);
+
+#endif
