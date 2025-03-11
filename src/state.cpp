48664c3a661f5ee36af92ddf70a459bbc07c73f2
91tleg <maxchou0417@gmail.com>
2025-03-11 14:45:12 -0700

    Add file -Initialize all pages to first page

---
+++ src/state.cpp
@@ -0,0 +1,7 @@
+#include "state.h"
+
+MENU menu_curr_page = MENU::SCAN_MODE;
+SCAN scan_curr_page = SCAN::SENSOR_ONE;
+PARAMS params_curr_page = PARAMS::PARAMS_ONE;
+DIAG diag_curr_page = DIAG::IN;
+RACE race_curr_page = RACE::TW;
