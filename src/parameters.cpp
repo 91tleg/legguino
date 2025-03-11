abbe0e9f2fdf43f4cd9910f4679cd13403eaa70f
91tleg <maxchou0417@gmail.com>
2025-03-11 14:13:24 -0700

    Add file -Initialize all struct parameters to zero.

---
+++ src/parameters.cpp
@@ -0,0 +1,8 @@
+#include "parameters.h"
+
+ecu_params ecu_parameters = {0};
+input_switches status = {0};
+io_switches status0 = {0};
+trouble_code_one status1 = {0};
+trouble_code_two status2 = {0};
+trouble_code_three status3 = {0};
