844b09bb7fde2bb3b952fb74b08eb885c7dbdeac
91tleg <maxchou0417@gmail.com>
2025-03-12 14:28:34 -0700

    move includes from source to header file for better modularity

--- src/state_race.cpp
+++ src/state_race.cpp
@@ -1,32 +1,32 @@
 #include "state_race.h"
-#include "btn.h"
-#include "ssm1.h"
-#include "legguino.h"
-#include "state_params.h"
-
-RACE race_current = RACE::TW;
 
 void race_page_control(void)
 {
-    if (race_current > RACE::MP)
-        race_current = RACE::TW;
-    btn1 = digitalRead(BUTTON1_PIN);
-    if (btn1 == LOW && btn1_prev == HIGH)
+    unsigned long previousMillis = 0;
+    unsigned long interval = 100;
+    unsigned long currentMillis = millis(); // Get current time
+    if (currentMillis - previousMillis >= interval)
     {
-        PORTB |= (1 << PB7);
-        _delay_ms(500);
-        PORTB &= ~(1 << PB7);
-        race_current = static_cast<RACE>(static_cast<uint8_t>(race_current) + 1);
+        if (race_curr_page > RACE::MP)
+        race_curr_page = RACE::TW;
+        btn1_read();
+        if (btn1 == LOW && btn1_prev == HIGH)
+        {
+            PORTB |= (1 << PB7);
+            _delay_ms(50);
+            PORTB &= ~(1 << PB7);
+            race_curr_page = static_cast<RACE>(static_cast<uint8_t>(race_curr_page) + 1);
+        }
+        btn1_prev = btn1;
     }
-    btn1_prev = digitalRead(BUTTON1_PIN);
 }
 
 void lcd_print_tw(void)
 {
     lcd.setCursor(0, 0);
-    sprintf(buffer, "Temp W:%df     ", ecu_parameters.tw);
+    sprintf(buffer, "Temp W:%df      ", ecu_parameters.tw);
     lcd.print(buffer);
-    lbg.drawValue(ecu_parameters.tw, -50, 275);
+    lbg.drawValue(ecu_parameters.tw, -47, 401);
 }
 
 void lcd_print_tps(void)
