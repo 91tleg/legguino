5daddc74039877f8270e93b840d85e94c8e1bba8
91tleg <maxchou0417@gmail.com>
2025-03-12 14:32:51 -0700

    add includes, move page enum to state.h

--- src/state_race.h
+++ src/state_race.h
@@ -2,19 +2,14 @@
 #define STATE_RACE_H
 
 #include <Arduino.h>
-
-enum class RACE : uint8_t
-{
-  /* 00 */ TW,
-  /* 01 */ TPS,
-  /* 02 */ MP,
-};
+#include "parameters.h"
+#include "state.h"
+#include "utilities.h"
+#include "btn.h"
 
 void race_page_control(void);
 void lcd_print_tw(void);
 void lcd_print_tps(void);
 void lcd_print_mp(void);
 
-extern RACE race_current;
-
-#endif // STATE_RACE
+#endif // STATE_RACE_H
