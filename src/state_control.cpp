7d10826333f65516b0198c11a0170169dcf69c1e
91tleg <maxchou0417@gmail.com>
2025-03-16 21:07:20 -0700

    move page control logic to state_control

--- src/state_control.cpp
+++ src/state_control.cpp
@@ -1,18 +1,45 @@
-#include "state_scan.h"
-#include "btn.h"
-SCAN lcd_current_page = SCAN::SENSOR_ONE;
+#include "state_control.h"
 
 void scan_page_control(void)
 {
-    if (lcd_current_page > SCAN::CLEAR_TC)
-        lcd_current_page = SCAN::SENSOR_ONE;
-    btn1 = digitalRead(BUTTON1_PIN);
-    if (btn1 == LOW && btn1_prev == HIGH)
+    if (btn1_pressed()) 
     {
-        PORTB |= (1 << PB7);
-        _delay_ms(100);
-        PORTB &= ~(1 << PB7);
-        lcd_current_page = static_cast<SCAN>(static_cast<uint8_t>(lcd_current_page) + 1);
+        trigger_feedback_one();
+        scan_curr_page = static_cast<SCAN>(static_cast<uint8_t>(scan_curr_page) + 1);
+        if (scan_curr_page > SCAN::CLEAR_TC)
+            scan_curr_page = SCAN::SENSOR_ONE;
     }
-    btn1_prev = digitalRead(BUTTON1_PIN);
+}
+
+void diag_page_control(void)
+{
+    if (btn1_pressed()) 
+    {
+        trigger_feedback_one();
+        diag_curr_page = static_cast<DIAG>(static_cast<uint8_t>(diag_curr_page) + 1);
+        if (diag_curr_page > DIAG::CLEAR)
+            diag_curr_page = DIAG::IN;
+    }
+}
+
+void params_page_control(void)
+{
+    if (btn1_pressed())
+    {
+        trigger_feedback_one();
+        params_curr_page = static_cast<PARAMS>(static_cast<uint8_t>(params_curr_page) + 1);
+        if (params_curr_page > PARAMS::PARAMS_EIGHT)
+            params_curr_page = PARAMS::PARAMS_ONE;
+    }
+}
+
+void race_page_control(void)
+{
+        if(btn1_pressed())
+        {
+            trigger_feedback_one();
+            race_curr_page = static_cast<RACE>(static_cast<uint8_t>(race_curr_page) + 1);
+            if (race_curr_page > RACE::MP)
+                race_curr_page = RACE::TW;
+        }
 }
--- src/state_control.h
+++ src/state_control.h
@@ -2,31 +2,13 @@
 #define STATE_SCAN_H
 
 #include <Arduino.h>
-#include "state_diag.h"
+#include "btn.h"
+#include "state.h"
+#include "utilities.h"
 
-enum class SCAN : uint8_t 
-{
-  /* 00 */ SENSOR_ONE,
-  /* 01 */ SENSOR_TWO,
-  /* 02 */ SENSOR_THREE,
-  /* 03 */ SENSOR_FOUR,
-  /* 04 */ SENSOR_FIVE,
-  /* 05 */ SENSOR_SIX,
-  /* 06 */ SENSOR_SEVEN,
-  /* 07 */ SENSOR_EIGHT,
-  /* 08 */ INPUT_SW,
-  /* 09 */ IO_SW,
-  /* 10 */ ACTIVE_TC_ONE,
-  /* 11 */ ACTIVE_TC_TWO,
-  /* 12 */ ACTIVE_TC_THREE,
-  /* 13 */ STORED_TC_ONE,
-  /* 14 */ STORED_TC_TWO,
-  /* 15 */ STORED_TC_THREE,
-  /* 16 */ CLEAR_TC
-};
-
+void diag_page_control(void);
+void params_page_control(void);
+void race_page_control(void);
 void scan_page_control(void);
-
-extern SCAN lcd_current_page;
 
 #endif // STATE_SCAN_H
