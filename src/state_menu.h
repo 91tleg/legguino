8a6d38820f0eb1e561101fd3fc0a2d6c8d4ce953
91tleg <maxchou0417@gmail.com>
2025-04-20 08:59:43 -0700

    fix: remove menu enum

--- src/state_menu.h
+++ src/state_menu.h
@@ -3,19 +3,14 @@
 
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
-
-enum class MENU : uint8_t
-{
-  /* 00 */ SCAN_MODE,
-  /* 01 */ PARAMS_MODE,
-  /* 02 */ DIAG_MODE,
-  /* 03 */ RACE_MODE,
-};
+#include "state_diag.h"
+#include "state_control.h"
+#include "state_params.h"
+#include "state_race.h"
+#include "btn.h"
 
 void display_menu(void);
 void select_mode(void);
 void check_return_menu(void);
-
-extern MENU menu_current;
 
 #endif // STAE_MENU_H
